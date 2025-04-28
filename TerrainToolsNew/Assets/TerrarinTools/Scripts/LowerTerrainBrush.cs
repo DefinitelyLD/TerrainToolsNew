53f86c0ca5950809eb4fbaed60a5cd0c19de384b
Madasir Ali <madasirali672@gmail.com>
2025-04-28 19:57:14 +0500

    cpu code added for diapatching flat,lower,smooth brush

--- TerrainToolsNew/Assets/TerrarinTools/Scripts/LowerTerrainBrush.cs
+++ TerrainToolsNew/Assets/TerrarinTools/Scripts/LowerTerrainBrush.cs
@@ -1,21 +1,52 @@
 ﻿using System;
+using UnityEngine;
 
 namespace TerrainTools {
     [TerrainBrush]
     public sealed class LowerTerrainBrush : TerrainBrush {
         public override BrushType GetBrushType() {
-            throw new NotImplementedException();
+            return BrushType.Heightmap;
         }
+
         public override void OnBrushDown(IBrushContext context) {
-            throw new NotImplementedException();
+
+            var commandBuffer = context.GetCommandBuffer();
+            var computeShader = context.GetCompute();
+
+            var brushHeightmapTexture = context.GetRenderTexture(ContextConstants.TerrainBrushHeightTexture);
+            var outputBrushHeightmapTexture = context.GetRenderTexture(ContextConstants.TerrainBrushHeightmapResultTexture);
+            var brushShapeTexture = context.GetRenderTexture(ContextConstants.TerrainBrushMaskTexture);
+            var terrainHeightmapTexture = context.GetRenderTexture(ContextConstants.TerrainHeightmapTexture);
+
+            var brushData = context.GetBrushData();
+
+            var heightmapSize = new Vector2Int(terrainHeightmapTexture.width, terrainHeightmapTexture.height);
+
+            commandBuffer.SetComputeTextureParam(computeShader, (int)KernelIndicies.LowerBrush, "TerrainHeightmapTexture", terrainHeightmapTexture);
+            commandBuffer.SetComputeTextureParam(computeShader, (int)KernelIndicies.LowerBrush, "BrushHeightmapTexture", brushHeightmapTexture);
+            commandBuffer.SetComputeTextureParam(computeShader, (int)KernelIndicies.LowerBrush, "BrushMaskTexture", brushShapeTexture);
+            commandBuffer.SetComputeTextureParam(computeShader, (int)KernelIndicies.LowerBrush, "OutputBrushHeightmapTexture", outputBrushHeightmapTexture);
+
+            commandBuffer.SetComputeFloatParam(computeShader, "BrushStrength", brushData.brushStrength);
+            commandBuffer.SetComputeFloatParam(computeShader, "BrushAngle", brushData.angle);
+            commandBuffer.SetComputeFloatParam(computeShader, "DeltaTime", brushData.deltaTime);
+
+            commandBuffer.SetComputeIntParams(computeShader, "BrushPosition", brushData.brushPosition.x, brushData.brushPosition.y);
+            commandBuffer.SetComputeIntParams(computeShader, "BrushSize", brushData.brushSize.x, brushData.brushSize.y);
+            commandBuffer.SetComputeIntParams(computeShader, "ActualBrushSize", brushData.actualBrushSize.x, brushData.actualBrushSize.y);
         }
 
         public override void OnBrushUp(IBrushContext context) {
-            throw new NotImplementedException();
         }
 
         public override void OnBrushUpdate(IBrushContext context) {
-            throw new NotImplementedException();
+
+            var commandBuffer = context.GetCommandBuffer();
+            var computeShader = context.GetCompute();
+
+            var dispatchSize = context.GetDispatchSize();
+
+            commandBuffer.DispatchCompute(computeShader, (int)KernelIndicies.LowerBrush, dispatchSize.x, dispatchSize.y, dispatchSize.z);
         }
     }
 }
