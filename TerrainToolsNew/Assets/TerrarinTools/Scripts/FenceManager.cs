4456db67ca864387863703e8497574a0a0232c9d
Madasir Ali <madasirali672@gmail.com>
2025-04-30 23:44:15 +0500

    the gpu and cpu sync are ruining everything,

--- TerrainToolsNew/Assets/TerrarinTools/Scripts/FenceManager.cs
+++ TerrainToolsNew/Assets/TerrarinTools/Scripts/FenceManager.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics;
+using UnityEngine;
 using UnityEngine.Rendering;
+using Debug = UnityEngine.Debug;
 
 namespace TerrainTools {
     public class FenceManager {
         private readonly (GraphicsFence fence, bool valid)[] m_fences;
         private int m_lastFence = -1;
+
+        private readonly Stopwatch m_stopwach;
 
         public void RegisterFence(GraphicsFence fence) {
             m_lastFence++;
@@ -23,6 +27,23 @@ namespace TerrainTools {
         }
 
         public bool IsFencePassed() {
+            var passed = IsFencePassedNonBlock();
+
+            if (passed == false) {
+                m_stopwach.Reset();
+                m_stopwach.Start();
+                while (!passed) {
+                    passed = IsFencePassedNonBlock();
+                }
+                m_stopwach.Stop();
+                Debug.LogWarning($"FenceManager: {m_stopwach.ElapsedMilliseconds} ms " +
+                    $"| {(m_stopwach.ElapsedTicks / (double)Stopwatch.Frequency) * 1000000} microseconds " +
+                    $"| {(m_stopwach.ElapsedTicks / (double)Stopwatch.Frequency) * 1000000000} ns to pass the fence. CPU was blocked.");
+            }
+            return true;
+        }
+
+        private bool IsFencePassedNonBlock() {
             bool result = true;
 
             for (int i = 0; i < m_fences.Length; i++) {
@@ -31,15 +52,16 @@ namespace TerrainTools {
 
                 if (m_fences[i].fence.passed) {
                     m_fences[i].valid = false;
-                } else if(result) {
+                } else if (result) {
                     result = false;
                 }
             }
-            return true;
+            return result;
         }
 
         public FenceManager(int maxFences = 2) {
             m_fences = new (GraphicsFence fence, bool valid)[maxFences];
+            m_stopwach = new();
         }
     }
 }
